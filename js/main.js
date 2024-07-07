3eaa2e415bec92ed7105e250cca4685d0bd5abbf
owlstechbd <owlstech.net@gmail.com>
2024-07-07 10:49:18 +0600

    home page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
   const video = document.getElementById('background-video');
   const sources = video.getElementsByTagName('source');
@@ -12,6 +11,32 @@ document.addEventListener("DOMContentLoaded", function() {
   video.play().catch(error => {
       console.log("Autoplay error:", error);
       // Handle autoplay error if needed
+  });
+});
+
+//owlCarousel
+
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+      items: 3,
+      loop: true,
+      margin: 10,
+      nav: true,
+      navText: ["<span class='owl-prev'>&#x276E;</span>", "<span class='owl-next'>&#x276F;</span>"],
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 2
+          },
+          1000: {
+              items: 3
+          }
+      }
   });
 });
 
